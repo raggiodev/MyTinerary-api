f14c9a96f689083c2d3e0e3bfbf697c80697a16d
JoeTheorium <fernandoandresraggio@gmail.com>
2023-08-23 00:52:14 -0300

    Probando otra forma para que no crashee nodemon

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.use('/api', indexRouter);
+// server.use('/api', indexRouter);
+server.get('/', (req, res, next) => {
+  res.send('Welcome to my API of "MyTinerary"! (Don´t get too comfy)')
+})
 
 server.listen(process.env['PORT'], () => {
-  console.log('server running on http://localhost:' + process.env['PORT']);
+  console.log('Server is running on: http://localhost:' + process.env['PORT']);
 });
