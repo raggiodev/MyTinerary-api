a875265d88dfda1e904b60da0ad09fe1964acbd7
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-04 14:57:38 -0300

    export itinerariesRouter

--- routers/itinerariesRouter.js
+++ routers/itinerariesRouter.js
@@ -6,3 +6,5 @@ const itinerariesRouter = Router();
 // Creación de routers para obtener los itinerarios por 'city'
 itinerariesRouter.post('/:cityId/itineraries', itinerariesController.createItinerary);
 itinerariesRouter.get('/:cityId/itineraries', itinerariesController.getItinerariesByCity);
+
+export default itinerariesRouter;
