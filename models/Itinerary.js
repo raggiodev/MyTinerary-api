e682e7369966307ef234e0bd6dd0fadef74069c8
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-04 13:19:23 -0300

    Upgrade Itinerary model

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -2,8 +2,10 @@ import {Schema, model} from 'mongoose';
 
 const itinerarySchema = Schema({
   title: { type: String, required: true },
+  photo: { type: String, required: true },
   author: { type: String, required: true },
-  price: { type: Number, required: true },
+  authorPhoto: { type: String, required: true },
+  price: { type: Number, required: true, min: 1, max: 5},
   duration: { type: Number, required: true },
   likes: { type: Number, default: 0 },
   hashtags: [{ type: String }],
