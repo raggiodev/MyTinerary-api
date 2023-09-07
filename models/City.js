0648a75b16a7130ee24444c2e023f331efb513fd
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-07 19:38:13 -0300

    Changed Fundation 4 Foundation. Changes in API with Postman

--- models/City.js
+++ models/City.js
@@ -5,7 +5,7 @@ const citySchema = Schema(
     city: { type: String, required: true, unique: true },
     country: { type: String, required: true },
     description: { type: String, required: true },
-    fundation: { type: String, required: true },
+    foundation: { type: String, required: true },
     population: { type: Number, required: true },
     photo: { type: String, required: true },
     itineraries: [{ type: Schema.Types.ObjectId, ref: "Itinerary" }], // Referencia al model 'Itinerary'
