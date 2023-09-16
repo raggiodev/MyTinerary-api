978a34f792f9845f6e5bcf753dc6671978adfe4f
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-15 21:37:59 -0300

    indexRouter fixed

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,16 +1,18 @@
 import {Router} from "express";
 import citiesRouter from "./citiesRouter.js";
 import itinerariesRouter from "./itinerariesRouter.js";
+import activitiesRouter from "./activitiesRouter.js";
 import authRouter from "./authRouter.js";
 
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res, next) => {
-  res.send("Welcome to my API: MyTinerary API");
+  response.send('Welcome to the API of MyTinerary, try the routes http://localhost:'+process.env['PORT']+'/api/cities or http://localhost:'+process.env['PORT']+'/api/itineraries or http://localhost:'+process.env['PORT']+'/api/auth for users');
 });
 
 indexRouter.use("/cities", citiesRouter);
 indexRouter.use("/itineraries", itinerariesRouter);
+indexRouter.use('/activities', activitiesRouter)
 indexRouter.use("/auth", authRouter);
 
 export default indexRouter;
